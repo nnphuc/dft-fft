a2de94c4f5d26db07db3550a9d5859516a21d9bd
Nguyen Ngoc Phuc <nnp.uet@gmail.com>
2017-02-28 19:48:00 +0700

    Update dft.py

--- dft.py
+++ dft.py
@@ -12,4 +12,12 @@ def dft(a):
   return mul(v,a)
 
 def idft(a):
-  
+  n=len(a)
+  invv=[]
+  w=e**(2*math.pi/n*1j)
+  for i in range(n):
+    invv.append([])
+    for j in range(n):
+      invv[-1]*append(w**(j*k)/n)
+  return mul(invv,a)
+
