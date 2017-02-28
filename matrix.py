ae9d5c4f1aa3b2782c78592b45172602930361c2
Nguyen Ngoc Phuc <nnp.uet@gmail.com>
2017-02-28 19:36:31 +0700

    Update matrix.py
    matrix multiple

--- matrix.py
+++ matrix.py
@@ -1,1 +1,17 @@
 
+def mul(A,B):
+  m=len(A)
+  n=len(A[0])
+  p=len(B)
+  q=len(B[0])
+  assert(n==p)
+  
+  C=[]
+  for i in range(m):
+    C.append([])
+    for j in range(q):
+      s=0
+      for k in range(n):
+        s+=A[i][k]*A[k][j]
+       C[-1].append(s)
+  return C
