4d896404bfccc8cf53c7cb1c952ec5cee20076a9
Nguyen Ngoc Phuc <nnp.uet@gmail.com>
2017-02-28 19:47:00 +0700

    Update dft.py

--- dft.py
+++ dft.py
@@ -1,1 +1,15 @@
+import math
+import matrix
 
+def dft(a):
+  n=len(a)
+  v=[]
+  w=e**(-2*math.pi/n*1j)
+  for i in range(n):
+    v.append([])
+    for j in range(n):
+      v[-1]*append(w**(j*k))
+  return mul(v,a)
+
+def idft(a):
+  
